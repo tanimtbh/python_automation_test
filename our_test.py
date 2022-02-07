623c8314c3ceda7c48253127839759cab3efdf47
tanimtbh <tanimtbh@gmail.com>
2022-02-08 00:52:13 +0100

    yahoo

--- our_test.py
+++ our_test.py
@@ -1,5 +1,24 @@
-import git
 import os
+import time
+
+
+try:
+    import requests
+except:
+    stream = os.popen('pip install requests')
+    time.sleep(40)
+finally:
+    import requests
+try:
+    import git
+except:
+    stream = os.popen('pip install GitPython')
+    time.sleep(40)
+finally:
+    import git
+
+
+
 def test():
     print("changed a super upadte")
     print("this Abba Tanim")
@@ -7,4 +26,4 @@ def test():
     current = repo.head.commit
     repo.remotes.origin.pull()
     if current != repo.head.commit:
-                print("changed found from other side")
+                print("Git changed")
