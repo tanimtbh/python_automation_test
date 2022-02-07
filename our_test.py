1a2086aaaa5dabc4461c3c4c9a9f9b8c0d3b9356
tanimtbh <tanimtbh@gmail.com>
2022-02-08 00:02:28 +0100

    again

--- our_test.py
+++ our_test.py
@@ -4,4 +4,4 @@ repo = git.Repo(os.getcwd())
 current = repo.head.commit
 repo.remotes.origin.pull()
 if current != repo.head.commit:
-            print("changed found")
+            print("changed found from other side")
