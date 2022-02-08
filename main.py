b12f62396051ac6237100e409940c51ebb6191bc
tanimtbh <tanimtbh@gmail.com>
2022-02-08 02:26:07 +0100

    soya

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 
 while True:
     try:
-        stream = os.popen('python pulllfile.pyw')
-        if(stream.read()==''):
+        if os.name in ('nt', 'dos'):
+            stream = os.popen('python pulllfile.pyw')
+        else:
             stream = os.popen('python3 pulllfile.pyw')
         print(stream.read())
     except:
