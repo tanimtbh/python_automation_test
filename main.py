03cd4a6040c6d7a73fb390da21400c7de59019f4
tanimtbh <tanimtbh@gmail.com>
2022-02-08 02:28:19 +0100

    aaaa

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 while True:
     try:
         if os.name in ('nt', 'dos'):
-            stream = os.popen('python pulllfile.pyw')
+            stream = os.popen('python pullfile.pyw')
         else:
-            stream = os.popen('python3 pulllfile.pyw')
+            stream = os.popen('python3 pullfile.pyw')
         print(stream.read())
     except:
         print("error found in main block")
