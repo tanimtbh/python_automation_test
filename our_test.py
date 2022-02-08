a1593ec46518f770c16248b2d8e5ccbaa9c9b3af
tanimtbh <tanimtbh@gmail.com>
2022-02-08 01:09:54 +0100

    yahoooha

--- our_test.py
+++ our_test.py
@@ -20,7 +20,7 @@ finally:
 
 
 def test():
-    print("Hi from test()")
+    print("from test() changed")
     repo = git.Repo(os.getcwd())
     current = repo.head.commit
     repo.remotes.origin.pull()
