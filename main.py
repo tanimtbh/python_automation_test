b27b593ed9e34259f20a1c7bce580b51d448b7ca
tanimtbh <tanimtbh@gmail.com>
2022-02-08 01:33:33 +0100

    dthedh

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 while True:
     try:
         stream = os.popen('python our_test.py')
+        print(stream.read())
     except:
         stream = os.popen('python3 our_test.py')
     finally:
